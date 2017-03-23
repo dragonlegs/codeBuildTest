cefe4cb5336123c5fc93c3d223d3b6c360ef8504
Hemanth <hemanthande16@gmail.com>
2017-03-23 11:43:30 -0400

    first commit

---
+++ OpenSH.sh
@@ -0,0 +1,3 @@
+IP=$(curl http://checkip.amazonaws.com)/32
+echo $IP of CodeBuild Instance
+aws ec2 authorize-security-group-ingress --group-id sg-a1d565de --protocol tcp --port 80 --cidr $IP --region us-east-1
